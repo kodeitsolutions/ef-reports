b687fc345021a91a5f786bad283e3d55a09d6ac7
gs-kodeit <gsanchez@kodeit.com.ve>
2016-03-30 13:56:51 -0430

    Creacion de formato de ordenes de compras sin precios (CGS)

---
+++ CGS_fOrdenes_ComprasSP.aspx.vb
@@ -0,0 +1,124 @@
+﻿Imports System.Data
+Partial Class CGS_fOrdenes_ComprasSP
+    Inherits vis2formularios.frmReporte
+
+    Dim loObjetoReporte As CrystalDecisions.CrystalReports.Engine.ReportDocument
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Try
+
+            Dim loComandoSeleccionar As New StringBuilder()
+
+            loComandoSeleccionar.AppendLine(" SELECT    Ordenes_Compras.Cod_Pro, ")
+            loComandoSeleccionar.AppendLine("           (CASE WHEN (Proveedores.Generico = 0 AND Ordenes_Compras.Nom_Pro = '') THEN Proveedores.Nom_Pro ELSE ")
+            loComandoSeleccionar.AppendLine("               (CASE WHEN (Ordenes_Compras.Nom_Pro = '') THEN Proveedores.Nom_Pro ELSE Ordenes_Compras.Nom_Pro END) END) AS  Nom_Pro, ")
+            loComandoSeleccionar.AppendLine("           (CASE WHEN (Proveedores.Generico = 0 AND Ordenes_Compras.Nom_Pro = '') THEN Proveedores.Rif ELSE ")
+            loComandoSeleccionar.AppendLine("               (CASE WHEN (Ordenes_Compras.Rif = '') THEN Proveedores.Rif ELSE Ordenes_Compras.Rif END) END) AS  Rif, ")
+            loComandoSeleccionar.AppendLine("           Proveedores.Nit, ")
+            loComandoSeleccionar.AppendLine("           (CASE WHEN (Proveedores.Generico = 0 AND Ordenes_Compras.Nom_Pro = '') THEN SUBSTRING(Proveedores.Dir_Fis,1, 200) ELSE ")
+            loComandoSeleccionar.AppendLine("               (CASE WHEN (SUBSTRING(Ordenes_Compras.Dir_Fis,1, 200) = '') THEN SUBSTRING(Proveedores.Dir_Fis,1, 200) ELSE SUBSTRING(Ordenes_Compras.Dir_Fis,1, 200) END) END) AS  Dir_Fis, ")
+            loComandoSeleccionar.AppendLine("           (CASE WHEN (Proveedores.Generico = 0 AND Ordenes_Compras.Nom_Pro = '') THEN Proveedores.Telefonos ELSE ")
+            loComandoSeleccionar.AppendLine("               (CASE WHEN (Ordenes_Compras.Telefonos = '') THEN Proveedores.Telefonos ELSE Ordenes_Compras.Telefonos END) END) AS  Telefonos, ")
+            
+            loComandoSeleccionar.AppendLine("           Proveedores.Correo, ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Nom_Pro         As Nom_Gen, ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Rif             As Rif_Gen, ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Dir_Fis         As Dir_Gen, ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Telefonos       As Tel_Gen, ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Documento, ")
+
+            loComandoSeleccionar.AppendLine("           Renglones_OCompras.Cod_Uni, ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Fec_Ini, ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Fec_Fin, ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Comentario, ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Status, ")
+            loComandoSeleccionar.AppendLine("           Formas_Pagos.Nom_For, ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Cod_Ven, ")
+            loComandoSeleccionar.AppendLine("           Renglones_OCompras.Cod_Art, ")
+            loComandoSeleccionar.AppendLine("           Articulos.Nom_Art               AS Nom_Art, ")
+            loComandoSeleccionar.AppendLine("			Articulos.Generico              AS Generico,")
+            loComandoSeleccionar.AppendLine("			Renglones_OCompras.Notas        AS Notas,")
+            loComandoSeleccionar.AppendLine("           Renglones_OCompras.Renglon, ")
+            loComandoSeleccionar.AppendLine("           Renglones_OCompras.Can_Art1, ")
+            loComandoSeleccionar.AppendLine("           Renglones_OCompras.Comentario   As Comentario_Renglon ")
+            loComandoSeleccionar.AppendLine(" FROM      Ordenes_Compras, ")
+            loComandoSeleccionar.AppendLine("           Renglones_OCompras, ")
+            loComandoSeleccionar.AppendLine("           Proveedores, ")
+            loComandoSeleccionar.AppendLine("           Formas_Pagos, ")
+            loComandoSeleccionar.AppendLine("           Articulos ")
+            loComandoSeleccionar.AppendLine(" WHERE     Ordenes_Compras.Documento   =   Renglones_OCompras.Documento AND ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Cod_Pro     =   Proveedores.Cod_Pro AND ")
+            loComandoSeleccionar.AppendLine("           Ordenes_Compras.Cod_For     =   Formas_Pagos.Cod_For AND ")
+            loComandoSeleccionar.AppendLine("           Articulos.Cod_Art           =   Renglones_OCompras.Cod_Art AND " & cusAplicacion.goFormatos.pcCondicionPrincipal)
+
+            'Me.mEscribirConsulta(loComandoSeleccionar.ToString())
+
+            Dim loServicios As New cusDatos.goDatos
+
+            Dim laDatosReporte As DataSet = loServicios.mObtenerTodosSinEsquema(loComandoSeleccionar.ToString, "curReportes")
+
+            '--------------------------------------------------'
+            ' Carga la imagen del logo en cusReportes            '
+            '--------------------------------------------------'
+            Me.mCargarLogoEmpresa(laDatosReporte.Tables(0), "LogoEmpresa")
+
+            '-------------------------------------------------------------------------------------------------------
+            ' Verificando si el select (tabla nº0) trae registros
+            '-------------------------------------------------------------------------------------------------------
+
+            If (laDatosReporte.Tables(0).Rows.Count <= 0) Then
+                Me.WbcAdministradorMensajeModal.mMostrarMensajeModal("Información", _
+                                          "No se Encontraron Registros para los Parámetros Especificados. ", _
+                                           vis3Controles.wbcAdministradorMensajeModal.enumTipoMensaje.KN_Informacion, _
+                                           "350px", _
+                                           "200px")
+            End If
+
+
+            loObjetoReporte = cusAplicacion.goFormatos.mCargarInforme("CGS_fOrdenes_ComprasSP", laDatosReporte)
+
+            Me.mTraducirReporte(loObjetoReporte)
+
+            Me.mFormatearCamposReporte(loObjetoReporte)
+
+            Me.crvCGS_fOrdenes_ComprasSP.ReportSource = loObjetoReporte
+
+        Catch loExcepcion As Exception
+
+            Me.WbcAdministradorMensajeModal.mMostrarMensajeModal("Error", _
+                          "No se pudo Completar el Proceso: " & loExcepcion.Message, _
+                           vis3Controles.wbcAdministradorMensajeModal.enumTipoMensaje.KN_Error, _
+                           "auto", _
+                           "auto")
+
+        End Try
+
+    End Sub
+
+    Protected Sub Page_Unload(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Unload
+
+        Try
+
+            loObjetoReporte.Close()
+
+        Catch loExcepcion As Exception
+
+        End Try
+
+    End Sub
+
+End Class
+'-------------------------------------------------------------------------------------------'
+' Fin del codigo
+'-------------------------------------------------------------------------------------------'
+' JJD: 08/11/08: Programacion inicial
+'-------------------------------------------------------------------------------------------'
+' CMS: 10/09/09: Se ajusto el nombre del articulo para los casos de aquellos articulos gen.
+'-------------------------------------------------------------------------------------------'
+' JJD: 09/01/10: Se cambio para que leyera datos de genericos de la Cotizacion cuando aplique
+'-------------------------------------------------------------------------------------------'
+' CMS: 17/03/10: Se aplicaron los metodos carga de imagen y validacion de registro cero
+'-------------------------------------------------------------------------------------------'
+' MAT: 02/09/11: Adición de Comentario en Renglones
+'-------------------------------------------------------------------------------------------'
