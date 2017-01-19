f0c55513344a9d385fb3cb412c1910a405980f1c
gs-kodeit <gsanchez@kodeit.com.ve>
2017-01-19 13:39:45 -0400

    Listado de gestion de ordenes de compras (CGS)

---
+++ CGS_rOCompras_Gestion.aspx.vb
@@ -0,0 +1,142 @@
+﻿'-------------------------------------------------------------------------------------------'
+' Inicio del codigo
+'-------------------------------------------------------------------------------------------'
+' Importando librerias 
+'-------------------------------------------------------------------------------------------'
+Imports System.Data
+
+'-------------------------------------------------------------------------------------------'
+' Inicio de clase "CGS_rOCompras_Gestion"
+'-------------------------------------------------------------------------------------------'
+Partial Class CGS_rOCompras_Gestion
+    Inherits vis2Formularios.frmReporte
+
+    Dim loObjetoReporte As CrystalDecisions.CrystalReports.Engine.ReportDocument
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim lcParametro0Desde As String = goServicios.mObtenerCampoFormatoSQL(cusAplicacion.goReportes.paParametrosIniciales(0), goServicios.enuOpcionesRedondeo.KN_FechaInicioDelDia)
+        Dim lcParametro0Hasta As String = goServicios.mObtenerCampoFormatoSQL(cusAplicacion.goReportes.paParametrosFinales(0), goServicios.enuOpcionesRedondeo.KN_FechaFinDelDia)
+        Dim lcParametro1Desde As String = goServicios.mObtenerCampoFormatoSQL(cusAplicacion.goReportes.paParametrosIniciales(1))
+        Dim lcParametro1Hasta As String = goServicios.mObtenerCampoFormatoSQL(cusAplicacion.goReportes.paParametrosFinales(1))
+
+        Dim lcOrdenamiento As String = cusAplicacion.goReportes.pcOrden
+
+        Dim lcComandoSeleccionar As New StringBuilder()
+
+        Try
+            lcComandoSeleccionar.AppendLine("DECLARE @ldFecha_Desde AS DATETIME = " & lcParametro0Desde)
+            lcComandoSeleccionar.AppendLine("DECLARE @ldFecha_Hasta AS DATETIME = " & lcParametro0Hasta)
+            lcComandoSeleccionar.AppendLine("DECLARE @lcCodUsu_Desde AS VARCHAR(8) = " & lcParametro1Desde)
+            lcComandoSeleccionar.AppendLine("DECLARE @lcCodUsu_Hasta AS VARCHAR(8) = " & lcParametro1Hasta)
+            lcComandoSeleccionar.AppendLine("")
+            lcComandoSeleccionar.AppendLine("SELECT	DISTINCT")
+            lcComandoSeleccionar.AppendLine("   	Ordenes_Compras.Documento, ")
+            lcComandoSeleccionar.AppendLine("		Ordenes_Compras.Cod_Pro, ")
+            lcComandoSeleccionar.AppendLine("		Ordenes_Compras.Status, ")
+            lcComandoSeleccionar.AppendLine("		Ordenes_Compras.Comentario, ")
+            lcComandoSeleccionar.AppendLine("		Renglones_OCompras.Renglon, ")
+            lcComandoSeleccionar.AppendLine("		Renglones_OCompras.Cod_Uni,")
+            lcComandoSeleccionar.AppendLine("		Renglones_OCompras.Notas,")
+            lcComandoSeleccionar.AppendLine("		Renglones_OCompras.Comentario   AS Com_Ren, ")
+            lcComandoSeleccionar.AppendLine("		Proveedores.Nom_Pro, ")
+            lcComandoSeleccionar.AppendLine("		Ordenes_Compras.Fec_Ini, ")
+            lcComandoSeleccionar.AppendLine("		Renglones_OCompras.Cod_Art,")
+            lcComandoSeleccionar.AppendLine("		Articulos.Nom_Art,")
+            lcComandoSeleccionar.AppendLine("		Articulos.Generico,")
+            lcComandoSeleccionar.AppendLine("		Renglones_OCompras.Cod_Alm, ")
+            lcComandoSeleccionar.AppendLine("       COALESCE ((SELECT SUM(Renglones_Recepciones.Can_Art1)")
+            lcComandoSeleccionar.AppendLine("                   FROM Renglones_Recepciones")
+            lcComandoSeleccionar.AppendLine("                   WHERE Renglones_Recepciones.Doc_Ori = Renglones_OCompras.Documento")
+            lcComandoSeleccionar.AppendLine("                   AND Renglones_Recepciones.Ren_Ori = Renglones_OCompras.Renglon),0) AS Cant_Recibida,")
+            lcComandoSeleccionar.AppendLine("       Renglones_OCompras.Precio1      AS Precio,")
+            lcComandoSeleccionar.AppendLine("		Renglones_OCompras.Can_Art1, ")
+            lcComandoSeleccionar.AppendLine("       Renglones_OCompras.Mon_Bru      AS Monto,")
+            lcComandoSeleccionar.AppendLine("		CONCAT(CONVERT(VARCHAR(12),CAST(@ldFecha_Desde AS DATE),103), ' - ',  CONVERT(VARCHAR(12),CAST(@ldFecha_Hasta AS DATE),103))	AS Fecha,")
+            lcComandoSeleccionar.AppendLine("		CASE WHEN @lcCodUsu_Desde <> ''")
+            lcComandoSeleccionar.AppendLine("			 THEN (SELECT Nom_Usu FROM Factory_Global.dbo.Usuarios WHERE Cod_Usu = @lcCodUsu_Desde)")
+            lcComandoSeleccionar.AppendLine("			 ELSE '' END				AS Usu_Desde,")
+            lcComandoSeleccionar.AppendLine("		CASE WHEN @lcCodUsu_Hasta <> 'zzzzzzz'")
+            lcComandoSeleccionar.AppendLine("			 THEN (SELECT Nom_Usu FROM Factory_Global.dbo.Usuarios WHERE Cod_Usu = @lcCodUsu_Hasta)")
+            lcComandoSeleccionar.AppendLine("			 ELSE '' END				AS Usu_Hasta")
+            lcComandoSeleccionar.AppendLine("FROM	Ordenes_Compras ")
+            lcComandoSeleccionar.AppendLine("	JOIN Renglones_OCompras ON Ordenes_Compras.Documento = Renglones_OCompras.Documento ")
+            lcComandoSeleccionar.AppendLine("	JOIN Proveedores ON Ordenes_Compras.Cod_Pro = Proveedores.Cod_Pro")
+            lcComandoSeleccionar.AppendLine("	JOIN Articulos ON Renglones_OCompras.Cod_Art =	Articulos.Cod_Art")
+            lcComandoSeleccionar.AppendLine("	JOIN Auditorias ON Ordenes_Compras.Documento = Auditorias.Documento")
+            lcComandoSeleccionar.AppendLine("		AND Auditorias.Tabla = 'Ordenes_Compras'")
+            lcComandoSeleccionar.AppendLine("		AND Auditorias.Accion = 'Confirmar'")
+            lcComandoSeleccionar.AppendLine("WHERE Auditorias.Registro BETWEEN @ldFecha_Desde AND @ldFecha_Hasta")
+            lcComandoSeleccionar.AppendLine("	AND Auditorias.Cod_Usu BETWEEN @lcCodUsu_Desde AND @lcCodUsu_Hasta")
+
+            'Me.mEscribirConsulta(lcComandoSeleccionar.ToString())
+
+            Dim loServicios As New cusDatos.goDatos
+
+            Dim laDatosReporte As DataSet = loServicios.mObtenerTodosSinEsquema(lcComandoSeleccionar.ToString, "curReportes")
+
+            Me.mCargarLogoEmpresa(laDatosReporte.Tables(0), "LogoEmpresa")
+
+            '-------------------------------------------------------------------------------------------------------
+            ' Verificando si el select (tabla nº0) trae registros
+            '-------------------------------------------------------------------------------------------------------
+
+            If (laDatosReporte.Tables(0).Rows.Count <= 0) Then
+                Me.WbcAdministradorMensajeModal.mMostrarMensajeModal("Información", _
+                                          "No se Encontraron Registros para los Parámetros Especificados. ", _
+                                           vis3Controles.wbcAdministradorMensajeModal.enumTipoMensaje.KN_Informacion, _
+                                           "350px", _
+                                           "200px")
+            End If
+
+
+            loObjetoReporte = cusAplicacion.goReportes.mCargarReporte("CGS_rOCompras_Gestion", laDatosReporte)
+
+            Me.mTraducirReporte(loObjetoReporte)
+
+            Me.mFormatearCamposReporte(loObjetoReporte)
+
+            Me.crvCGS_rOCompras_Gestion.ReportSource = loObjetoReporte
+
+        Catch loExcepcion As Exception
+
+            Me.WbcAdministradorMensajeModal.mMostrarMensajeModal("Error", _
+                          "No se pudo Completar el Proceso: " & loExcepcion.Message, _
+                           vis3Controles.wbcAdministradorMensajeModal.enumTipoMensaje.KN_Error, _
+                           "auto", _
+                           "auto")
+
+        End Try
+
+    End Sub
+
+    Protected Sub Page_Unload(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Unload
+        Try
+
+            loObjetoReporte.Close()
+
+        Catch loExcepcion As Exception
+
+
+        End Try
+    End Sub
+End Class
+'-------------------------------------------------------------------------------------------'
+' JJD: 14/10/08: Programacion inicial
+'-------------------------------------------------------------------------------------------'
+' CMS: 20/04/09: se agregaron las condiciones: Ordenes_Compras.Fec_Ini, Proveedores.nom_pro y Ordenes_Compras.status
+'-------------------------------------------------------------------------------------------'
+' YJP: 14/05/09: Agregar filtro revisión
+'-------------------------------------------------------------------------------------------'
+' CMS: 18/06/09: Metodo de ordenamiento
+'-------------------------------------------------------------------------------------------'
+' AAP:  01/07/09: Filtro "Sucursal:"
+'-------------------------------------------------------------------------------------------'
+' CMS: 22/07/09: Filtro BackOrder, lo conllevo al anexo del campo Can_Pen1,
+'                 verificacion de registros
+'-------------------------------------------------------------------------------------------'
+' CMS:  13/08/09: Se Agrego la restricción Renglones_Pedidos.Can_Pen1 <> 0 cuando el filtro 
+'                   BackOrder = BackOrder
+'-------------------------------------------------------------------------------------------'
+' CMS: 19/03/10: se agrego el filtro cod_art
+'-------------------------------------------------------------------------------------------'
