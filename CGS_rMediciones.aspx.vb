e33e611037f072eca01e0a0d03ac1edafac0390c
gs-kodeit <gsanchez@kodeit.com.ve>
2017-03-16 15:54:04 -0400

    Listado de Mediciones (CGS)

---
+++ CGS_rMediciones.aspx.vb
@@ -0,0 +1,112 @@
+﻿'-------------------------------------------------------------------------------------------'
+' Inicio del codigo
+'-------------------------------------------------------------------------------------------'
+' Importando librerias 
+'-------------------------------------------------------------------------------------------'
+Imports System.Data
+
+'-------------------------------------------------------------------------------------------'
+' Inicio de clase "CGS_rMediciones"
+'-------------------------------------------------------------------------------------------'
+Partial Class CGS_rMediciones
+    Inherits vis2formularios.frmReporte
+
+    Dim loObjetoReporte As CrystalDecisions.CrystalReports.Engine.ReportDocument
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Try
+            Dim lcParametro0Desde As String = goServicios.mObtenerCampoFormatoSQL(cusAplicacion.goReportes.paParametrosIniciales(0), goServicios.enuOpcionesRedondeo.KN_FechaInicioDelDia)
+            Dim lcParametro0Hasta As String = goServicios.mObtenerCampoFormatoSQL(cusAplicacion.goReportes.paParametrosFinales(0), goServicios.enuOpcionesRedondeo.KN_FechaFinDelDia)
+            Dim lcParametro1Desde As String = goServicios.mObtenerCampoFormatoSQL(cusAplicacion.goReportes.paParametrosIniciales(1))
+            Dim lcParametro1Hasta As String = goServicios.mObtenerCampoFormatoSQL(cusAplicacion.goReportes.paParametrosFinales(1))
+            Dim lcParametro2Desde As String = goServicios.mObtenerCampoFormatoSQL(cusAplicacion.goReportes.paParametrosIniciales(2))
+            Dim lcParametro2Hasta As String = goServicios.mObtenerCampoFormatoSQL(cusAplicacion.goReportes.paParametrosFinales(2))
+
+            Dim lcOrdenamiento As String = cusAplicacion.goReportes.pcOrden
+
+            Dim loComandoSeleccionar As New StringBuilder()
+
+            loComandoSeleccionar.AppendLine("DECLARE @ldFechaDesde	DATETIME = " & lcParametro0Desde)
+            loComandoSeleccionar.AppendLine("DECLARE @ldFechaHasta	DATETIME = " & lcParametro0Hasta)
+            loComandoSeleccionar.AppendLine("DECLARE @lcArtDesde	VARCHAR(10) = " & lcParametro1Desde)
+            loComandoSeleccionar.AppendLine("DECLARE @lcArtHasta	VARCHAR(10) = " & lcParametro1Hasta)
+            loComandoSeleccionar.AppendLine("DECLARE @lcOrigenDesde	VARCHAR(30) = " & lcParametro2Desde)
+            loComandoSeleccionar.AppendLine("DECLARE @lcOrigenHasta	VARCHAR(30) = " & lcParametro2Hasta)
+            loComandoSeleccionar.AppendLine("")
+            loComandoSeleccionar.AppendLine("SELECT	Mediciones.Documento			AS Documento,")
+            loComandoSeleccionar.AppendLine("		Mediciones.Cod_Art				AS Articulo,")
+            loComandoSeleccionar.AppendLine("		Mediciones.Cod_Alm				AS Almacen,")
+            loComandoSeleccionar.AppendLine("		Mediciones.Origen				AS Origen,")
+            loComandoSeleccionar.AppendLine("		Mediciones.Cod_Reg				AS Num_Origen,")
+            loComandoSeleccionar.AppendLine("		Mediciones.Adicional			AS Adicional,")
+            loComandoSeleccionar.AppendLine("		Renglones_Mediciones.Cod_Var	AS Variable,")
+            loComandoSeleccionar.AppendLine("		Renglones_Mediciones.Res_Num	AS Valor")
+            loComandoSeleccionar.AppendLine("FROM Mediciones")
+            loComandoSeleccionar.AppendLine("	JOIN Renglones_Mediciones ON Mediciones.Documento = Renglones_Mediciones.Documento")
+            loComandoSeleccionar.AppendLine("       AND Renglones_Mediciones.Cod_Var LIKE '%'+'PDESP' ")
+            loComandoSeleccionar.AppendLine("WHERE Mediciones.Fecha BETWEEN @ldFechaDesde AND @ldFechaHasta")
+            loComandoSeleccionar.AppendLine("	AND Mediciones.Cod_Art BETWEEN @lcArtDesde AND @lcArtHasta")
+            loComandoSeleccionar.AppendLine("	AND Mediciones.Origen BETWEEN @lcOrigenDesde AND @lcOrigenHasta")
+
+            'Me.mEscribirConsulta(loComandoSeleccionar.ToString())
+
+            Dim loServicios As New cusDatos.goDatos
+
+            Dim laDatosReporte As DataSet = loServicios.mObtenerTodosSinEsquema(loComandoSeleccionar.ToString, "curReportes")
+
+            Me.mCargarLogoEmpresa(laDatosReporte.Tables(0), "LogoEmpresa")
+
+            '-------------------------------------------------------------------------------------------------------
+            ' Verificando si el select (tabla nº0) trae registros
+            '-------------------------------------------------------------------------------------------------------
+
+            If (laDatosReporte.Tables(0).Rows.Count <= 0) Then
+                Me.WbcAdministradorMensajeModal.mMostrarMensajeModal("Información", _
+                                          "No se Encontraron Registros para los Parámetros Especificados. ", _
+                                           vis3Controles.wbcAdministradorMensajeModal.enumTipoMensaje.KN_Informacion, _
+                                           "350px", _
+                                           "200px")
+            End If
+
+            loObjetoReporte = cusAplicacion.goReportes.mCargarReporte("CGS_rMediciones", laDatosReporte)
+
+            Me.mTraducirReporte(loObjetoReporte)
+
+            Me.mFormatearCamposReporte(loObjetoReporte)
+
+            Me.crvCGS_rMediciones.ReportSource = loObjetoReporte
+
+        Catch loExcepcion As Exception
+
+            Me.WbcAdministradorMensajeModal.mMostrarMensajeModal("Error", _
+                          "No se pudo Completar el Proceso: " & loExcepcion.Message, _
+                           vis3Controles.wbcAdministradorMensajeModal.enumTipoMensaje.KN_Error, _
+                          "auto", _
+                           "auto")
+
+        End Try
+
+    End Sub
+
+    Protected Sub Page_Unload(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Unload
+
+        Try
+
+            loObjetoReporte.Close()
+
+        Catch loExcepcion As Exception
+
+
+        End Try
+
+    End Sub
+End Class
+'-------------------------------------------------------------------------------------------'
+' Fin del codigo
+'-------------------------------------------------------------------------------------------'
+' MJP: 16/07/08: Codigo inicial
+'-------------------------------------------------------------------------------------------'
+' GS:  14/03/16: Cambio a Listado de Artículos.
+'-------------------------------------------------------------------------------------------'
+
