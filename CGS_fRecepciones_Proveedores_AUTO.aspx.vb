540100a5829877ee6a4232350cff612c015ebddc
gs-kodeit <gsanchez@kodeit.com.ve>
2017-08-04 09:56:14 -0400

    Ajustar formato para que muestre ajuste segun nuevo proceso de registro. Formato de Nota de Recepcion normal y AUTO

--- CGS_fRecepciones_Proveedores_AUTO.aspx.vb
+++ CGS_fRecepciones_Proveedores_AUTO.aspx.vb
@@ -11,109 +11,125 @@ Partial Class CGS_fRecepciones_Proveedores_AUTO
 
         Dim loComandoSeleccionar As New StringBuilder()
 
-        loComandoSeleccionar.AppendLine("SELECT Recepciones.Cod_Pro                     AS Cod_Pro, ")
-        loComandoSeleccionar.AppendLine("       Proveedores.Nom_Pro                     AS Nom_Pro, ")
-        loComandoSeleccionar.AppendLine("       Proveedores.Rif                         AS Rif, ")
-        loComandoSeleccionar.AppendLine("       Proveedores.Dir_Fis                     AS Dir_Fis, ")
-        loComandoSeleccionar.AppendLine("       Proveedores.Telefonos                   AS Telefonos, ")
-        loComandoSeleccionar.AppendLine("	    Recepciones.Documento					AS Documento,")
-        loComandoSeleccionar.AppendLine("	    Recepciones.Status					    AS Status,")
-        loComandoSeleccionar.AppendLine("	    Recepciones.Fec_Ini						AS Fec_Ini,")
-        loComandoSeleccionar.AppendLine("	    Recepciones.Comentario					AS Comentario,")
-        loComandoSeleccionar.AppendLine("	    Recepciones.Notas					    AS Datos,")
-        'loComandoSeleccionar.AppendLine("	    Renglones_Recepciones.Renglon			AS Renglon,")
-        loComandoSeleccionar.AppendLine("	    Renglones_Recepciones.Cod_Art			AS Cod_Art,")
-        loComandoSeleccionar.AppendLine("	    Articulos.Nom_Art						AS Nom_Art,")
-        loComandoSeleccionar.AppendLine("       Articulos.Generico						AS Generico,")
-        loComandoSeleccionar.AppendLine("       Renglones_Recepciones.Notas				AS Notas, ")
-        loComandoSeleccionar.AppendLine("       Renglones_Recepciones.Comentario        AS Peso, ")
-        loComandoSeleccionar.AppendLine("	    Renglones_Recepciones.Can_Art1          AS Cantidad,")
-        loComandoSeleccionar.AppendLine("	    Renglones_Recepciones.Cod_Uni			AS Cod_Uni,")
-        loComandoSeleccionar.AppendLine("		Renglones_Recepciones.Caracter1			AS Adicional,")
-        loComandoSeleccionar.AppendLine("	    Renglones_Recepciones.Doc_Ori			AS Doc_Ori,")
-        loComandoSeleccionar.AppendLine("       COALESCE(Operaciones_Lotes.Cod_Lot,'')	AS Lote,")
-        loComandoSeleccionar.AppendLine("       COALESCE(Operaciones_Lotes.Cantidad,0)	AS Cantidad_Lote,")
-        loComandoSeleccionar.AppendLine("		COALESCE(Piezas.Res_Num, 0)			    AS Piezas,")
-        loComandoSeleccionar.AppendLine("		COALESCE(Desperdicio.Res_Num, 0)		AS Porc_Desperdicio,")
-        loComandoSeleccionar.AppendLine("		COALESCE((Desperdicio.Res_Num*Operaciones_Lotes.Cantidad)/100, ")
-        loComandoSeleccionar.AppendLine("		(Desperdicio.Res_Num*Renglones_Recepciones.Can_Art1)/100, 0)	AS Cant_Desperdicio")
-        loComandoSeleccionar.AppendLine("INTO #tmpRecepcion")
-        loComandoSeleccionar.AppendLine("FROM Recepciones")
-        loComandoSeleccionar.AppendLine("   JOIN Renglones_Recepciones ON Recepciones.Documento = Renglones_Recepciones.Documento")
-        loComandoSeleccionar.AppendLine("	JOIN Proveedores ON Recepciones.Cod_Pro = Proveedores.Cod_Pro")
-        loComandoSeleccionar.AppendLine("	JOIN Articulos ON Articulos.Cod_Art = Renglones_Recepciones.Cod_Art ")
-        loComandoSeleccionar.AppendLine("   LEFT JOIN Operaciones_Lotes ON Operaciones_Lotes.Num_Doc = Renglones_Recepciones.Documento")
-        loComandoSeleccionar.AppendLine("       AND Renglones_Recepciones.Cod_Art = Operaciones_Lotes.Cod_Art")
-        loComandoSeleccionar.AppendLine("       AND Operaciones_Lotes.Ren_Ori = Renglones_Recepciones.Renglon")
-        loComandoSeleccionar.AppendLine("       AND Operaciones_Lotes.Tip_Doc = 'Recepciones' AND Operaciones_Lotes.Tip_Ope = 'Entrada'")
-        loComandoSeleccionar.AppendLine("   LEFT JOIN Mediciones ON Mediciones.Cod_Reg = Recepciones.Documento")
-        loComandoSeleccionar.AppendLine("       AND Mediciones.Origen = 'Recepciones'")
-        loComandoSeleccionar.AppendLine("       AND Mediciones.Adicional LIKE ('%'+RTRIM(Operaciones_Lotes.Cod_Lot)+'%')")
-        loComandoSeleccionar.AppendLine("       AND Renglones_Recepciones.Renglon = SUBSTRING(Mediciones.Adicional, LEN(Mediciones.Adicional), 1)")
-        loComandoSeleccionar.AppendLine("	LEFT JOIN Renglones_Mediciones AS Piezas ON Mediciones.Documento = Piezas.Documento")
-        loComandoSeleccionar.AppendLine("		AND Piezas.Cod_Var = 'NREC-NPIEZ'")
-        loComandoSeleccionar.AppendLine("	LEFT JOIN Renglones_Mediciones AS Desperdicio ON Mediciones.Documento = Desperdicio.Documento")
-        loComandoSeleccionar.AppendLine("		AND Desperdicio.Cod_Var = 'NREC-PDESP'")
-        loComandoSeleccionar.AppendLine(" WHERE " & cusAplicacion.goFormatos.pcCondicionPrincipal & ";")
-        loComandoSeleccionar.AppendLine("")
-        loComandoSeleccionar.AppendLine("WITH Sumatorias (Cod_Art, Lote,Cantidad, Cantidad_Lote) AS")
-        loComandoSeleccionar.AppendLine("(")
-        loComandoSeleccionar.AppendLine("   SELECT Cod_Art, Lote, SUM(Cantidad) AS Cantidad, SUM(Cantidad_Lote) AS Cantidad_Lote")
-        loComandoSeleccionar.AppendLine("   FROM #tmpRecepcion")
-        loComandoSeleccionar.AppendLine("   GROUP BY Cod_Art, Lote")
-        loComandoSeleccionar.AppendLine(")")
-        loComandoSeleccionar.AppendLine("UPDATE #tmpRecepcion ")
-        loComandoSeleccionar.AppendLine("SET #tmpRecepcion.Cantidad = Sumatorias.Cantidad, ")
-        loComandoSeleccionar.AppendLine("	#tmpRecepcion.Cantidad_Lote = Sumatorias.Cantidad_Lote")
-        loComandoSeleccionar.AppendLine("FROM #tmpRecepcion")
-        loComandoSeleccionar.AppendLine("	INNER JOIN Sumatorias ON Sumatorias.Cod_Art = #tmpRecepcion.Cod_Art")
-        loComandoSeleccionar.AppendLine("UPDATE #tmpRecepcion SET Cant_Desperdicio = (Cantidad_Lote * Porc_Desperdicio)/100 ")
-        loComandoSeleccionar.AppendLine("")
-        loComandoSeleccionar.AppendLine("SELECT * FROM #tmpRecepcion WHERE Adicional <> 'DECLARADO'")
-        loComandoSeleccionar.AppendLine("")
-        loComandoSeleccionar.AppendLine("DROP TABLE #tmpRecepcion")
-        loComandoSeleccionar.AppendLine("")
+            loComandoSeleccionar.AppendLine("SELECT Recepciones.Cod_Pro                     AS Cod_Pro, ")
+            loComandoSeleccionar.AppendLine("       Proveedores.Nom_Pro                     AS Nom_Pro, ")
+            loComandoSeleccionar.AppendLine("       Proveedores.Rif                         AS Rif, ")
+            loComandoSeleccionar.AppendLine("       Proveedores.Dir_Fis                     AS Dir_Fis, ")
+            loComandoSeleccionar.AppendLine("       Proveedores.Telefonos                   AS Telefonos, ")
+            loComandoSeleccionar.AppendLine("	    Recepciones.Documento					AS Documento,")
+            loComandoSeleccionar.AppendLine("	    Recepciones.Status					    AS Status,")
+            loComandoSeleccionar.AppendLine("	    Recepciones.Fec_Ini						AS Fec_Ini,")
+            loComandoSeleccionar.AppendLine("	    Recepciones.Comentario					AS Comentario,")
+            loComandoSeleccionar.AppendLine("	    Recepciones.Notas					    AS Datos,")
+            'loComandoSeleccionar.AppendLine("	    Renglones_Recepciones.Renglon			AS Renglon,")
+            loComandoSeleccionar.AppendLine("	    Renglones_Recepciones.Cod_Art			AS Cod_Art,")
+            loComandoSeleccionar.AppendLine("	    Articulos.Nom_Art						AS Nom_Art,")
+            loComandoSeleccionar.AppendLine("       Articulos.Generico						AS Generico,")
+            loComandoSeleccionar.AppendLine("       Renglones_Recepciones.Notas				AS Notas, ")
+            loComandoSeleccionar.AppendLine("		COALESCE((CASE WHEN Renglones_Recepciones.Comentario <> ''")
+            loComandoSeleccionar.AppendLine("                      THEN CONVERT(NUMERIC(18,2), REPLACE(REPLACE(Renglones_Recepciones.Comentario,'.',''), ',','.' ) )")
+            loComandoSeleccionar.AppendLine("                      ELSE 0")
+            loComandoSeleccionar.AppendLine("                 END) ,0)                      AS Peso,")
+            loComandoSeleccionar.AppendLine("	    Renglones_Recepciones.Can_Art1          AS Cantidad, ")
+            loComandoSeleccionar.AppendLine("	    Renglones_Recepciones.Cod_Uni			AS Cod_Uni, ")
+            loComandoSeleccionar.AppendLine("		Renglones_Recepciones.Caracter1			AS Adicional, ")
+            loComandoSeleccionar.AppendLine("	    Renglones_Recepciones.Doc_Ori			As Doc_Ori, ")
+            loComandoSeleccionar.AppendLine("       COALESCE(Operaciones_Lotes.Cod_Lot,'')	AS Lote,")
+            loComandoSeleccionar.AppendLine("       COALESCE(Operaciones_Lotes.Cantidad,0)	AS Cantidad_Lote,")
+            loComandoSeleccionar.AppendLine("		COALESCE(Piezas.Res_Num, 0)			    AS Piezas,")
+            loComandoSeleccionar.AppendLine("		COALESCE(Desperdicio.Res_Num, 0)		AS Porc_Desperdicio,")
+            loComandoSeleccionar.AppendLine("		COALESCE((Desperdicio.Res_Num*Operaciones_Lotes.Cantidad)/100, ")
+            loComandoSeleccionar.AppendLine("		(Desperdicio.Res_Num*Renglones_Recepciones.Can_Art1)/100, 0)	AS Cant_Desperdicio,")
+            loComandoSeleccionar.AppendLine("       COALESCE(Ajustes.Documento,'')			AS Ajuste,")
+            loComandoSeleccionar.AppendLine("       COALESCE(Renglones_Ajustes.Can_Art1,0)	AS Cantidad_Ajuste,")
+            loComandoSeleccionar.AppendLine("       CONCAT(COALESCE(RTRIM(Renglones_Ajustes.Cod_Tip),''),' - ', COALESCE(Tipos_Ajustes.Nom_Tip,''))	AS Tipo_Ajuste,")
+            loComandoSeleccionar.AppendLine("		CONCAT(COALESCE(RTRIM(Renglones_Recepciones.Cod_Alm),''),' - ',COALESCE(Almacenes.Nom_Alm,''))		AS Nom_Alm")
+            loComandoSeleccionar.AppendLine("")
+            loComandoSeleccionar.AppendLine("INTO #tmpRecepcion")
+            loComandoSeleccionar.AppendLine("FROM Recepciones")
+            loComandoSeleccionar.AppendLine("   JOIN Renglones_Recepciones ON Recepciones.Documento = Renglones_Recepciones.Documento")
+            loComandoSeleccionar.AppendLine("	JOIN Proveedores ON Recepciones.Cod_Pro = Proveedores.Cod_Pro")
+            loComandoSeleccionar.AppendLine("	JOIN Articulos ON Articulos.Cod_Art = Renglones_Recepciones.Cod_Art ")
+            loComandoSeleccionar.AppendLine("   LEFT JOIN Operaciones_Lotes ON Operaciones_Lotes.Num_Doc = Renglones_Recepciones.Documento")
+            loComandoSeleccionar.AppendLine("       AND Renglones_Recepciones.Cod_Art = Operaciones_Lotes.Cod_Art")
+            loComandoSeleccionar.AppendLine("       AND Operaciones_Lotes.Ren_Ori = Renglones_Recepciones.Renglon")
+            loComandoSeleccionar.AppendLine("       AND Operaciones_Lotes.Tip_Doc = 'Recepciones' AND Operaciones_Lotes.Tip_Ope = 'Entrada'")
+            loComandoSeleccionar.AppendLine("   LEFT JOIN Mediciones ON Mediciones.Cod_Reg = Recepciones.Documento")
+            loComandoSeleccionar.AppendLine("       AND Mediciones.Origen = 'Recepciones'")
+            loComandoSeleccionar.AppendLine("       AND Mediciones.Adicional LIKE ('%'+RTRIM(Operaciones_Lotes.Cod_Lot)+'%')")
+            loComandoSeleccionar.AppendLine("       AND Renglones_Recepciones.Renglon = SUBSTRING(Mediciones.Adicional, LEN(Mediciones.Adicional), 1)")
+            loComandoSeleccionar.AppendLine("	LEFT JOIN Renglones_Mediciones AS Piezas ON Mediciones.Documento = Piezas.Documento")
+            loComandoSeleccionar.AppendLine("		AND Piezas.Cod_Var = 'NREC-NPIEZ'")
+            loComandoSeleccionar.AppendLine("	LEFT JOIN Renglones_Mediciones AS Desperdicio ON Mediciones.Documento = Desperdicio.Documento")
+            loComandoSeleccionar.AppendLine("		AND Desperdicio.Cod_Var = 'NREC-PDESP'")
+            loComandoSeleccionar.AppendLine("	LEFT JOIN Ajustes ON Ajustes.Doc_Ori = Recepciones.Documento")
+            loComandoSeleccionar.AppendLine("		AND Ajustes.Tip_Ori = 'Recepciones'")
+            loComandoSeleccionar.AppendLine("	LEFT JOIN Renglones_Ajustes ON Ajustes.Documento = Renglones_Ajustes.Documento")
+            loComandoSeleccionar.AppendLine("	LEFT JOIN Almacenes ON Renglones_Ajustes.Cod_Alm = Almacenes.Cod_Alm")
+            loComandoSeleccionar.AppendLine("	LEFT JOIN Tipos_Ajustes ON Tipos_Ajustes.Cod_Tip = Renglones_Ajustes.Cod_Tip")
+            loComandoSeleccionar.AppendLine(" WHERE " & cusAplicacion.goFormatos.pcCondicionPrincipal & ";")
+            loComandoSeleccionar.AppendLine("")
+            loComandoSeleccionar.AppendLine("WITH Sumatorias (Cod_Art, Lote, Cantidad, Cantidad_Lote, Peso) AS")
+            loComandoSeleccionar.AppendLine("(")
+            loComandoSeleccionar.AppendLine("   SELECT Cod_Art, Lote,SUM(Cantidad) AS Cantidad, SUM(Cantidad_Lote) AS Cantidad_Lote, SUM(Peso) AS Peso")
+            loComandoSeleccionar.AppendLine("   FROM #tmpRecepcion")
+            loComandoSeleccionar.AppendLine("   GROUP BY Cod_Art, Lote")
+            loComandoSeleccionar.AppendLine(")")
+            loComandoSeleccionar.AppendLine("UPDATE #tmpRecepcion ")
+            loComandoSeleccionar.AppendLine("SET #tmpRecepcion.Cantidad = Sumatorias.Cantidad, ")
+            loComandoSeleccionar.AppendLine("	#tmpRecepcion.Cantidad_Lote = Sumatorias.Cantidad_Lote,")
+            loComandoSeleccionar.AppendLine("	#tmpRecepcion.Peso = Sumatorias.Peso")
+            loComandoSeleccionar.AppendLine("FROM #tmpRecepcion")
+            loComandoSeleccionar.AppendLine("	INNER JOIN Sumatorias ON Sumatorias.Cod_Art = #tmpRecepcion.Cod_Art")
+            loComandoSeleccionar.AppendLine("WHERE #tmpRecepcion.Lote = Sumatorias.Lote")
+            loComandoSeleccionar.AppendLine("   AND #tmpRecepcion.Cod_Art = Sumatorias.Cod_Art")
+            loComandoSeleccionar.AppendLine("UPDATE #tmpRecepcion SET Cant_Desperdicio = (Cantidad_Lote * Porc_Desperdicio)/100 ")
+            loComandoSeleccionar.AppendLine("")
+            loComandoSeleccionar.AppendLine("SELECT * FROM #tmpRecepcion WHERE Adicional <> 'DECLARADO' ORDER BY Cod_Art, Lote")
+            loComandoSeleccionar.AppendLine("")
+            loComandoSeleccionar.AppendLine("DROP TABLE #tmpRecepcion")
+            loComandoSeleccionar.AppendLine("")
 
-        'Me.mEscribirConsulta(loComandoSeleccionar.ToString)
+            'Me.mEscribirConsulta(loComandoSeleccionar.ToString)
 
-        Dim loServicios As New cusDatos.goDatos
-        Dim laDatosReporte As DataSet = loServicios.mObtenerTodosSinEsquema(loComandoSeleccionar.ToString, "curReportes")
+            Dim loServicios As New cusDatos.goDatos
+            Dim laDatosReporte As DataSet = loServicios.mObtenerTodosSinEsquema(loComandoSeleccionar.ToString, "curReportes")
 
-        Me.mCargarLogoEmpresa(laDatosReporte.Tables(0), "LogoEmpresa")
+            Me.mCargarLogoEmpresa(laDatosReporte.Tables(0), "LogoEmpresa")
 
-        '-------------------------------------------------------------------------------------------------------
-        ' Verificando si el select (tabla nº0) trae registros
-        '-------------------------------------------------------------------------------------------------------
+            '-------------------------------------------------------------------------------------------------------
+            ' Verificando si el select (tabla nº0) trae registros
+            '-------------------------------------------------------------------------------------------------------
 
-        If (laDatosReporte.Tables(0).Rows.Count <= 0) Then
-            Me.WbcAdministradorMensajeModal.mMostrarMensajeModal("Información", _
-                  "No se Encontraron Registros para los Parámetros Especificados. ", _
-  vis3Controles.wbcAdministradorMensajeModal.enumTipoMensaje.KN_Informacion, _
-                   "350px", _
-                   "200px")
-        End If
-
-
-        '--------------------------------------------------'
-        ' Carga la imagen del logo en cusReportes            '
-        '--------------------------------------------------'
+            If (laDatosReporte.Tables(0).Rows.Count <= 0) Then
+                Me.WbcAdministradorMensajeModal.mMostrarMensajeModal("Información", _
+                      "No se Encontraron Registros para los Parámetros Especificados. ", _
+      vis3Controles.wbcAdministradorMensajeModal.enumTipoMensaje.KN_Informacion, _
+                       "350px", _
+                       "200px")
+            End If
 
 
-        loObjetoReporte = cusAplicacion.goFormatos.mCargarInforme("CGS_fRecepciones_Proveedores_AUTO", laDatosReporte)
+            '--------------------------------------------------'
+            ' Carga la imagen del logo en cusReportes            '
+            '--------------------------------------------------'
 
-        Me.mTraducirReporte(loObjetoReporte)
 
-        Me.mFormatearCamposReporte(loObjetoReporte)
+            loObjetoReporte = cusAplicacion.goFormatos.mCargarInforme("CGS_fRecepciones_Proveedores_AUTO", laDatosReporte)
 
-        Me.crvCGS_fRecepciones_Proveedores_AUTO.ReportSource = loObjetoReporte
+            Me.mTraducirReporte(loObjetoReporte)
 
-        'Catch loExcepcion As Exception
+            Me.mFormatearCamposReporte(loObjetoReporte)
 
-        '    Me.WbcAdministradorMensajeModal.mMostrarMensajeModal("Error", _
-        '              "No se pudo Completar el Proceso: " & loExcepcion.Message, _
-        '               vis3Controles.wbcAdministradorMensajeModal.enumTipoMensaje.KN_Error, _
-        '               "auto", _
-        '               "auto")
+            Me.crvCGS_fRecepciones_Proveedores_AUTO.ReportSource = loObjetoReporte
+
+            'Catch loExcepcion As Exception
+
+        'Me.WbcAdministradorMensajeModal.mMostrarMensajeModal("Error",
+        '          "No se pudo Completar el Proceso: " & loExcepcion.Message,
+        '           vis3Controles.wbcAdministradorMensajeModal.enumTipoMensaje.KN_Error,
+        '           "auto",
+        '           "auto")
 
         'End Try
 
